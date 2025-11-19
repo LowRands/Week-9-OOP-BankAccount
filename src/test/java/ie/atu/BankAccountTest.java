c27c54177d8c65b5dfb1658e857c09dae3b6f266
EthanLourens-STUDENT <G00467054@atu.ie>
2025-11-19 16:33:26 +0000

    Bank Account Test Code

--- src/test/java/ie/atu/BankAccountTest.java
+++ src/test/java/ie/atu/BankAccountTest.java
@@ -1,6 +1,35 @@
 package ie.atu;
 
-public class BankAccountTest {
+import ie.atu.week9.BankAccount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BankAccountTest {
+    BankAccount account;
+
+    @BeforeEach
+    void setup()
+    {
+        account = new BankAccount();
+    }
+
+    void constructorInitialisation()
+    {
+        account = new BankAccount("ACC12345", "Ethan", 100);
+        assertEquals("ACC12345", account.getACCNo());
+        assertEquals("Ethan", account.getName());
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+    void constructorInitialisation()
+    {
+       Exception ex = assertThrows(IllegalArgumentException.class, () -> new BankAccount("ACC12345", "Ethan",-100));
+       assertEquals("Balance must be greater than 0", ex.getMessage());
+    }
 }
+
 
